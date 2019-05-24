2bac7e36635eab8f4f42dca1e5a663766116c5af
Dylan <Dylan@PC-DNS>
2019-05-24 19:19:23 +0200

    JavaFx Controller "Dungeon" - IDs und Events hinzugefügt.
    Außerdem: Hintergrundbilder werden nun aus den css-Dateien
    direkt geladen, sodass diese sich immer an die aktuelle
    Fenstergröße anpassen.

--- src/de/pk/view/visuell/fxmlSzenenLayouts/AnwendungFX.java
+++ src/de/pk/view/visuell/fxmlSzenenLayouts/AnwendungFX.java
@@ -44,6 +44,7 @@ public class AnwendungFX extends Application
 	{
 		AnwendungFX.aktualisiereAufloesungsStyleSheets();
 		AnwendungFX.anwendungsStage.sizeToScene();
+		AnwendungFX.anwendungsStage.centerOnScreen();
 	}
 
 
@@ -57,9 +58,9 @@ public class AnwendungFX extends Application
 				.add(new Image(new File(Spielkonstanten.ANWENDUNG_ANWENDUNGS_ICON).toURI().toURL().toExternalForm()));
 		AnwendungFX.anwendungsStage.setTitle(DE_de.ANWENDUNG_FENSTER_TITEL);
 		AnwendungFX.anwendungsStage.setResizable(false);
+		AnwendungFX.anwendungsStage.centerOnScreen();
 		AnwendungFX.anwendungsStage.setOnCloseRequest(windowEvent -> Main.anwendungBeenden());
 		AnwendungFX.anwendungsStage.setScene(SZENEN_MAP.get(Spielkonstanten.ANWENDUNG_TITEL_SZENE));
-		AnwendungFX.anwendungsStage.centerOnScreen();
 		AnwendungFX.anwendungsStage.show();
 	}
 
